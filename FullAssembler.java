5eb4ffc3e9316dc5ea3d421cbacc00c85bd53cef
Jonathan Wong <jwong123@binghamton.edu>
2019-05-02 13:00:18 -0400

    added the loader file and started prelim work on fullassembler

---
+++ FullAssembler.java
@@ -0,0 +1,5 @@
+// this requires SimpleAssembler to be tested and fully working
+public class FullAssembler implements Assembler{
+    // calls SimpleAssembler's assemble
+    // supposed to take several days
+}
---
+++ Loader.java
@@ -0,0 +1,62 @@
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+public class Loader{
+    public static String load(Machine machine, File file) throws Exception{
+	if(machine == null | file == null){
+	    return null;
+	}
+
+	int numInstr = 0;
+	boolean flag = true;
+
+	try(DataInputStream input = new DataInputStream(new FileInputStream(file))){
+	    while(true){
+		x = input.readInt();
+		if(flag && x == -1){
+		    flag = !flag;
+		} else if(flag){
+		    numInstr += 1;
+		    machine.addCode(new Instruction((byte)x, input.readInt()));
+		} else{
+		    machine.setData(x, input.readInt());
+		}
+	    }
+	} catch(EOFException e){
+	    return "" + numInstr;
+	} catch(FileNotFoundException e1){
+	    return("File" + file.getName() + " Not Found");
+	} catch (Exception e){
+	    throw e;
+	}
+    }
+
+    public static void main(String[] args){
+	Machine machine = Machine(() -> System.exit(0));
+
+	String str = Loader.load(machine, new File("factorial.pexe"));
+	int total = Integer.parseInt(str);
+
+	// should see 20 lines
+	
+	for(int i = 0; i < total; i++){
+	    System.out.println(machine.getCode(i).getText());
+	}
+
+	System.out.println(Arrays.toString(machine.getData(0, 20)));
+
+	// should see 169 lines
+	
+	machine.setPC(0);
+	while(true){
+	    System.out.println(machine.getCode(machine.getPC()).getText());
+	    machine.step();
+	    System.out.println(Arrays.toString(machine.getData(0, 20)));
+	}
+    }
+}
+	
