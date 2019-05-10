5311e382d45bd5e7e6cafe40fccfb7ecfcdfbfb7
JonathanCen <43393783+JonathanCen@users.noreply.github.com>
2019-05-10 10:39:28 -0400

    Create MenuBarBuilder.java

---
+++ MenuBarBuilder.java
@@ -0,0 +1,69 @@
+package projectview;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+
+public class MenuBarBuilder {
+	
+	private JMenuItem assemble = new JMenuItem("Assemble Source...");
+	private JMenuItem load = new JMenuItem("Load File...");
+	private JMenuItem exit = new JMenuItem("Exit");
+	private JMenuItem go = new JMenuItem("Go");
+	private Mediator mediator;
+
+	public MenuBarBuilder(Mediator med) {
+		mediator = med;
+	}
+
+	//@Override
+	public void update() {
+		assemble.setEnabled(mediator.getCurrentState().getAssembleFileActive());
+		load.setEnabled(mediator.getCurrentState().getLoadFileActive());
+		go.setEnabled(mediator.getCurrentState().getStepActive());
+	}
+	
+	public JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.setMnemonic(KeyEvent.VK_F);
+		
+		JMenuItem assemble = new JMenuItem("Assemble Source...");
+		assemble.setMnemonic(KeyEvent.VK_M);
+		assemble.setAccelerator(KeyStroke.getKeyStroke(
+			KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+		assemble.addActionListener(e -> mediator.assembleFile());
+		menu.add(assemble);
+		
+		//JMenuItem load = new JMenuItem("Load Program...");
+		load.setMnemonic(KeyEvent.VK_L);
+		load.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK)); // have to change CTRL_MASK maybe
+		load.addActionListener(e -> mediator.loadFile()); 
+		menu.add(load);
+		
+		menu.addSeparator();
+		
+		//JMenuItem exit = new JMenuItem("Exit")
+		exit.setMnemonic(KeyEvent.VK_E);
+		exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		exit.addActionListener(e -> mediator.exit()); // have to change this part
+		menu.add(load);
+		
+		return menu;
+	}
+	
+	public JMenu createExeccuteMenu() {
+		JMenu menu = new JMenu("Execute");
+		menu.setMnemonic(KeyEvent.VK_X);
+		
+		go.setMnemonic(KeyEvent.VK_G);
+		go.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_G, ActionEvent.CTRL_MASK));
+		go.addActionListener(e -> mediator.execute()); // not sure if right
+		menu.add(load);
+		return menu;
+	}
+
+}
