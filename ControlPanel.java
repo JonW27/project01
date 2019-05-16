df0dca8cb27184a7d194e5e1f4ca570d3c16573c
JonathanCen <43393783+JonathanCen@users.noreply.github.com>
2019-05-16 18:01:13 -0400

    Update ControlPanel.java

--- ControlPanel.java
+++ ControlPanel.java
@@ -27,9 +27,17 @@ public class ControlPanel {
 		stepButton.addActionListener(e -> mediator.step());
 		panel.add(stepButton);
 		
+		clearButton.setBackground(Color.WHITE);
 		clearButton.addActionListener(e -> mediator.clear());
+		panel.add(clearButton);
+		
+		runButton.setBackground(Color.WHITE);
 		runButton.addActionListener(e -> mediator.toggleAutoStep());
+		panel.add(runButton);
+		
+		reloadButton.setBackground(Color.WHITE);
 		reloadButton.addActionListener(e -> mediator.reload());
+		panel.add(reloadButton);
 		
 		JSlider slider = new JSlider(5,1000);
 		slider.addChangeListener(e -> mediator.setPeriod(slider.getValue())); 
